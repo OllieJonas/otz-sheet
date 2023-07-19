a7451834c016242e77c30e076f012d832b52f91f
Ollie <olliejonas@gmail.com>
2023-07-19 19:03:28 +0100

    added ts stuff

---
+++ ts/interfaces.ts
@@ -0,0 +1,78 @@
+// UTIL
+export interface ValueWrapper<T> {
+    value: T
+}
+
+// SPREADSHEET
+export interface PerkColour {
+    red?: number
+    green?: number
+    blue?: number
+}
+
+export interface SpreadsheetPerk {
+    perk_tier: PerkColour
+    perk_name: string
+}
+
+export interface BaseSpreadsheetCharacter {
+    name: string
+    perk_tiers: Array<ValueWrapper<PerkColour>>
+    perk_names: Array<ValueWrapper<string>>
+    availability: string
+}
+
+export interface Guides {}
+
+interface KillerGuides extends Guides {
+    latest_tier_list: string,
+    which_killer: string,
+}
+
+interface SurvivorGuides extends Guides {
+
+}
+
+export interface Spreadsheet<T extends BaseSpreadsheetCharacter, V extends Guides> {
+    characters: Array<T>
+    universals: Array<SpreadsheetPerk>
+    guides: V
+}
+
+export interface SpreadsheetKiller {
+    movement_speed: string
+    terror_radius: string
+}
+
+export interface SpreadsheetSurvivor {
+    stealth: string
+    noise: string
+    cries: string
+}
+
+export interface Perk {
+    icon: string
+    description: string
+    is_upcoming_patch: boolean
+}
+
+export interface Survivor {
+    name: string
+    icon: string
+    image_half: string
+    image_full: string
+    wiki_link: string
+    lore: string
+}
+
+export interface Power {
+    name: string
+    icon: string
+    desc: string
+}
+
+export interface Killer extends Survivor {
+    former_name: string
+    height: string
+    power: Power
+}
---
+++ ts/page.ts
@@ -0,0 +1,11 @@
+import {BaseSpreadsheetCharacter, Guides, Spreadsheet, SpreadsheetKiller} from './interfaces'
+
+const GITHUB_BASE: string = "https://raw.githubusercontent.com/OllieJonas/scrape-dbd-perks/master/out/"
+const KILLER_SPREADSHEET_URL = GITHUB_BASE + "killer_spreadsheet_LATEST.json"
+
+async function loadSpreadsheet<T extends BaseSpreadsheetCharacter, V extends Guides>(url: string): Promise<Spreadsheet<T, V>> {
+    const response: Response = await fetch(url);
+    return await response.json();
+}
+
+loadSpreadsheet<SpreadsheetKiller>(KILLER_SPREADSHEET_URL).then(res => console.log(res))
